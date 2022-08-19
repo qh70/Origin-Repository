bfdd616080be97fea7a899defe7afccc815752b0
qh70 <qh7047789@gmail.com>
2022-08-20 02:25:08 +0800

    try to find out no switch for login icon

--- app.py
+++ app.py
@@ -216,85 +216,85 @@ def api_attraction_id(attractionId):
 	finally:
 		db_connection.close()
 ####
-# @app.route("/api/user")
-# def api_user_get():
-	# if session!={}:
-	# 	if session["email"]!="logout":
-	# 		try:
-	# 			db_connection=pool.get_connection()
-	# 			my_cursor=db_connection.cursor()
-	# 			my_cursor.execute("SELECT `id`,`name` FROM `user` WHERE email='%s'" %session["email"])
-	# 			result=my_cursor.fetchone()
-	# 			return jsonify({
-	# 				"data":{
-	# 					"id":result[0],
-	# 					"name":result[1],
-	# 					"email":session["email"]
-	# 				}
-	# 			})
-	# 		except:
-	# 			return jsonify({"error":True}), 500
-	# 		finally:
-	# 			db_connection.close()
-	# 	else:
-	# 		return jsonify({"data":None})
-	# else:
-	# 	return jsonify({"data":None})
+@app.route("/api/user")
+def api_user_get():
+	if session!={}:
+		if session["email"]!="logout":
+			try:
+				db_connection=pool.get_connection()
+				my_cursor=db_connection.cursor()
+				my_cursor.execute("SELECT `id`,`name` FROM `user` WHERE email='%s'" %session["email"])
+				result=my_cursor.fetchone()
+				return jsonify({
+					"data":{
+						"id":result[0],
+						"name":result[1],
+						"email":session["email"]
+					}
+				})
+			except:
+				return jsonify({"error":True}), 500
+			finally:
+				db_connection.close()
+		else:
+			return jsonify({"data":None})
+	else:
+		return jsonify({"data":None})
 	
-# @app.route("/api/user", methods=["POST",])
-# def api_user():
-# 	name_signup=request.json["name_signup"]
-# 	email_signup=request.json["email_signup"]
-# 	password_signup=request.json["password_signup"]
-# 	try:
-# 		db_connection=pool.get_connection()
-# 		my_cursor=db_connection.cursor()
-# 		my_cursor.execute("SELECT `name`,`email`,`password` FROM `user` WHERE email='%s'" %email_signup)
-# 		result=my_cursor.fetchone()
-# 		if result!=None:
-# 			return jsonify({"error":True,"message":"Email 已經被註冊"})
-# 		else:
-# 			my_cursor.execute("INSERT INTO user (name, email, password) VALUES (%s, %s, %s);", (name_signup, email_signup, password_signup))
-# 			db_connection.commit()
-# 			return jsonify({"ok":True}), 200
-# 	except:
-# 		return jsonify({"error":True}), 500
-# 	finally:
-# 		db_connection.close()
+@app.route("/api/user", methods=["POST",])
+def api_user():
+	name_signup=request.json["name_signup"]
+	email_signup=request.json["email_signup"]
+	password_signup=request.json["password_signup"]
+	try:
+		db_connection=pool.get_connection()
+		my_cursor=db_connection.cursor()
+		my_cursor.execute("SELECT `name`,`email`,`password` FROM `user` WHERE email='%s'" %email_signup)
+		result=my_cursor.fetchone()
+		if result!=None:
+			return jsonify({"error":True,"message":"Email 已經被註冊"})
+		else:
+			my_cursor.execute("INSERT INTO user (name, email, password) VALUES (%s, %s, %s);", (name_signup, email_signup, password_signup))
+			db_connection.commit()
+			return jsonify({"ok":True}), 200
+	except:
+		return jsonify({"error":True}), 500
+	finally:
+		db_connection.close()
 
-# @app.route("/api/user", methods=["PATCH"])
-# def api_user_patch():
-# 	email_login=request.json["email_login"]
-# 	password_login=request.json["password_login"]
-# 	try:
-# 		db_connection=pool.get_connection()
-# 		my_cursor=db_connection.cursor()
-# 		my_cursor.execute("SELECT `name`,`email`,`password` FROM `user` WHERE email='%s'" %email_login)
-# 		result=my_cursor.fetchone()
-# 		if result!=None:
-# 			# 登入成功
-# 			if password_login==result[2]:
-# 				session["email"]=result[1]
-# 				return jsonify({"ok":True}), 200
-# 			# 密碼錯誤
-# 			else:
-# 				return jsonify({"error":True,"message":"密碼錯誤"}), 400
-# 		# 此Email 未註冊帳號
-# 		else:
-# 			return jsonify({"error":True,"message":"此Email 未註冊帳號"}), 400
-# 	except:
-# 		return jsonify({"error":True}), 500
-# 	finally:
-# 		db_connection.close()
+@app.route("/api/user", methods=["PATCH"])
+def api_user_patch():
+	email_login=request.json["email_login"]
+	password_login=request.json["password_login"]
+	try:
+		db_connection=pool.get_connection()
+		my_cursor=db_connection.cursor()
+		my_cursor.execute("SELECT `name`,`email`,`password` FROM `user` WHERE email='%s'" %email_login)
+		result=my_cursor.fetchone()
+		if result!=None:
+			# 登入成功
+			if password_login==result[2]:
+				session["email"]=result[1]
+				return jsonify({"ok":True}), 200
+			# 密碼錯誤
+			else:
+				return jsonify({"error":True,"message":"密碼錯誤"}), 400
+		# 此Email 未註冊帳號
+		else:
+			return jsonify({"error":True,"message":"此Email 未註冊帳號"}), 400
+	except:
+		return jsonify({"error":True}), 500
+	finally:
+		db_connection.close()
 
-# @app.route("/api/user", methods=["DELETE"])
-# def api_user_delete():
-# 	session["email"]="logout"
-# 	return jsonify({"ok":True}), 200
+@app.route("/api/user", methods=["DELETE"])
+def api_user_delete():
+	session["email"]="logout"
+	return jsonify({"ok":True}), 200
 
-from view.api_user import api_User
+# from view.api_user import api_User
 
-app.register_blueprint(api_User)
+# app.register_blueprint(api_User)
 
 @app.route("/api/booking", methods=["POST"])
 def api_booking_post():
